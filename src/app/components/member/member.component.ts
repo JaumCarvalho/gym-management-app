9fadb0e8f9142f63d3e9185e8abd450a7bbac12f
João Carvalho <cjoao7610@gmail.com>
2025-04-20 15:44:21 -0300

    feat: create members page

---
+++ src/app/components/member/member.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-member',
+  templateUrl: './member.component.html',
+  styleUrls: ['./member.component.scss'],
+})
+export class MemberComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/tabs/members/members.page.ts
+++ src/app/pages/tabs/members/members.page.ts
@@ -1,18 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent, IonHeader, IonTitle, IonToolbar,
+  IonButton,IonButtons, IonIcon,
+  IonSearchbar, IonRow, IonCol, 
+  IonSelect, IonSelectOption, IonList, IonItemDivider } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircle, menuOutline, notifications } from 'ionicons/icons';
 
 @Component({
   selector: 'app-members',
   templateUrl: './members.page.html',
   styleUrls: ['./members.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonItemDivider, IonList, IonCol, IonRow, 
+    IonIcon,
+    IonButtons,
+    IonButton,
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonSearchbar,
+    IonRow,
+    IonCol,
+    IonSelect,
+    IonSelectOption
+  ]
 })
 export class MembersPage implements OnInit {
 
-  constructor() { }
+  constructor() {
+    addIcons({menuOutline,notifications,addCircle});
+}
 
   ngOnInit() {
   }
