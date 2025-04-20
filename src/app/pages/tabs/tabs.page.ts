852d38da1f9a14bbe3df8e60a878195febbfa577
João Carvalho <cjoao7610@gmail.com>
2025-04-20 13:01:48 -0300

    feat: add tab bar navigation

---
+++ src/app/pages/tabs/tabs.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { IonTabs, IonTabBar, IonTabButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {homeOutline, personOutline, peopleOutline, calendarOutline, home, person, people, calendar} from 'ionicons/icons';
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.page.html',
+  styleUrls: ['./tabs.page.scss'],
+  standalone: true,
+  imports: [IonIcon, IonTabButton, IonTabBar, IonTabs]
+})
+export class TabsPage implements OnInit {
+  currentTab = signal<string>('members');
+
+  constructor() {
+    addIcons({
+      homeOutline,
+      personOutline,
+      peopleOutline,
+      calendarOutline,
+      home,
+      person,
+      people,
+      calendar
+    }); 
+  }
+
+  ngOnInit() {}
+  getCurrentTab(event: {tab: string}){
+    console.log(event);
+    this.currentTab.set(event.tab);
+  }
+}
+
